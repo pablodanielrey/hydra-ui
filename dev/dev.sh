714fa65d10945c8fc9efa1de5ad492ed20724c4a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-06-10 14:37:51 -0300

    agrego el gitignore

--- dev/dev.sh
+++ dev/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "corriendo en el puerto 10002"
-docker run --rm -ti --name login-ui -v $(pwd)/src:/src -p 10002:4200 desarrollo-ui
+docker run --rm -ti --name login-ui -v $(pwd)/src:/src --user $(id -u):$(id -g) -p 10002:4200 desarrollo-ui
